ad9032ed83aca970e415db5b14526068b734cc18
Alex Elam <alexelamit@gmail.com>
2017-01-27 11:01:52 -0500

    added calculation - numbers are good

--- src/testCalcMortgage/Test.java
+++ src/testCalcMortgage/Test.java
@@ -21,6 +21,15 @@ public class Test {
 			int n; // number of payments
 			System.out.print("Enter number of monthly payments: ");
 			n = input.nextInt();
-			System.out.println(n);
+			//System.out.println(n);
+			
+			//float m; // monthly payment amount
+			//m = (r(1 + r) / (1 + r) - 1) * l 
+			float x = 1 + r;
+			//System.out.println(x);
+			
+			double m; // amount of monthly payment
+			m = (((Math.pow(x, n) * r) / (Math.pow(x, n) - 1)) * l);
+			System.out.printf("Monthly payment: %.2f", m);
 		}
 }
